3a4e6e0a7f1124256f4771269d5ec09a07e82fb1
Jason <jdoria702@gmail.com>
2025-05-09 22:53:02 -0700

    util package and DBConnection classes added

--- 157Project/src/main/java/util/DBConnection.java
+++ 157Project/src/main/java/util/DBConnection.java
@@ -6,12 +6,12 @@ import java.sql.SQLException;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/ecommerce";
-    private static final String USER = "root";  // Change if necessary
-    private static final String PASSWORD = "[REDACTED]";  // Change if necessary
+    private static final String USER = "root";
+    private static final String PASSWORD = "{REDACTED}";
 
     public static Connection getConnection() throws SQLException {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");  // Ensure you load the driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
